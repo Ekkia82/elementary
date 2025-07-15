a2dec867bd5fb7b3734148b654a413d4f6a1e58b
илья <vlasichev98@mail.ru>
2025-07-15 21:47:37 +0300

    1.7 Результат работы метода

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,20 +1,25 @@
 package ru.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        int six = 6;
-        int five = 5;
-        int four = 4;
-        int sixDivTwo = six / two;
-        System.out.println(sixDivTwo);
-        int fiveMinusTwo = five - two;
-        System.out.println(fiveMinusTwo);
-        int fourTimeTwo = four * two;
-        System.out.println(fourTimeTwo);
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 }
+
+    class MathFunc {
+
+        public static int func1(int x) {
+            int y = x;
+            return y;
+        }
+
+        public static void main(String[] args) {
+            Calculator.plus(1, 2);
+            Calculator.plus(10, 11);
+            int result3 = MathFunc.func1(100);
+            System.out.println(result3);
+        }
+    }
 
