71597cd2f485efb1253b57f46781b2691a695938
илья <vlasichev98@mail.ru>
2025-07-17 20:19:23 +0300

    1.16 Математические операторы.

---
+++ src/main/java/ru/condition/StringRunner.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        idea += " But I am a newbie. ";
+        int year = 2025;
+        idea += year;
+        System.out.println(idea);
+    }
+}
+
