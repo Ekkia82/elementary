1b4bc28e2c172abe3b0300e4b464fbdb6dacf685
илья <vlasichev98@mail.ru>
2025-07-18 19:40:40 +0300

    2.2 Что такое import

--- src/main/java/ru/calculator/MathCalculator.java
+++ src/main/java/ru/calculator/MathCalculator.java
@@ -2,7 +2,6 @@ package ru.calculator;
 
 import static ru.math.MathFunction.*;
 
-
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
@@ -22,7 +21,6 @@ public class MathCalculator {
                 +division(first, second)
                 +minus(first, second);
     }
-
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
